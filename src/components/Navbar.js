ca4075a57f14168a56ed959ddff6f5144cb29ac8
LuisPa <diganluispa@gmail.com>
2018-10-02 12:16:10 -0600

    first modify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2" style={{color:"#f40"}}>Blog.luispa</h1>
+          </div>
+          {/* <figure className="image">
             <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          </figure> */}
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
+        {/* <Link className="navbar-item" to="/about">
           About
         </Link>
         <Link className="navbar-item" to="/products">
           Products
-        </Link>
+        </Link> */}
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/luispagarcia"
           target="_blank"
           rel="noopener noreferrer"
         >
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,13 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+              <h1 className="has-text-weight-bold is-size-2">Últimas entradas</h1>
             </div>
             {posts
               .map(({ node: post }) => (
                 <div
                   className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                  style={{ border: '1px solid #eaecee', padding: '2em 2em' }}
                   key={post.id}
                 >
                   <p>
@@ -34,7 +34,7 @@ export default class IndexPage extends React.Component {
                     <br />
                     <br />
                     <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
+                      Seguir leyendo →
                     </Link>
                   </p>
                 </div>
