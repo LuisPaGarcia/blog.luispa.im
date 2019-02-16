f32c9340dcf07e71ec915a138db141fdd9f02815
LuisPa <diganluispa@gmail.com>
2019-02-16 16:40:06 -0600

    Adding context management.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,92 +1,117 @@
+//@ts-check
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import '../style.css';
+import { Context } from './context';
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
 
-export default class IndexPage extends React.Component {
-	state = {
-		visible: 'about'
-	};
+		this.up = (state) => {
+			this.setState({
+				visible: state
+			});
+		};
 
-	up = (state) => {
-		this.setState({
-			visible: state
-		});
-	};
+		this.state = {
+			visible: 'about',
+			up: this.up
+		};
+	}
+
 	render() {
-		const { data } = this.props;
-		const { visible } = this.state;
-		const { edges: posts } = data.allMarkdownRemark;
+		let value = {
+			visible: this.state.visible,
+			up: this.state.up,
+			posts: this.props.data.allMarkdownRemark.edges
+		};
 		return (
-			<Layout>
-				<div className="topMenu">
-					<span
-						className={`item-menu-class ${visible === 'about' ? ` active` : ``}`}
-						onClick={() => this.up('about')}
-					>
-						About
-					</span>
-					<span
-						className={`item-menu-class ${visible === 'blog' ? ` active` : ``}`}
-						onClick={() => this.up('blog')}
-					>
-						Blog
-					</span>
-				</div>
-				{!(visible === 'about') ? null : (
-					<section>
-						<h1>HOLA</h1>
-					</section>
-				)}
-				{!(visible === 'blog') ? null : (
-					<section>
-						<div>
-							<div>
-								<br />
-								<span className="ultimas-entradas">Últimas entradas</span>
-								<br />
+			<Context.Provider value={value}>
+				<IndexPage />
+			</Context.Provider>
+		);
+	}
+}
+
+class IndexPage extends React.Component {
+	render() {
+		return (
+			<Context.Consumer>
+				{({ visible, up, posts }) => {
+					return (
+						<Layout>
+							<div className="topMenu">
+								<span
+									className={`item-menu-class ${visible === 'about' ? ` active` : ``}`}
+									onClick={() => up('about')}
+								>
+									About
+								</span>
+								<span
+									className={`item-menu-class ${visible === 'blog' ? ` active` : ``}`}
+									onClick={() => up('blog')}
+								>
+									Blog
+								</span>
 							</div>
-							{posts.map(({ node: post }) => {
-								return (
-									<div className="box-post" key={post.id}>
-										<p>
-											<Link className="post-title-class" to={post.fields.slug}>
-												{post.frontmatter.title}
-											</Link>
+							{!(visible === 'about') ? null : (
+								<section>
+									<h1>HOLA</h1>
+								</section>
+							)}
+							{!(visible === 'blog') ? null : (
+								<section>
+									<div>
+										<div>
 											<br />
-											<small className="date-class">
-												{new Intl.DateTimeFormat('es-GT', {
-													year: 'numeric',
-													month: 'long'
-												}).format(Date.parse(post.frontmatter.date))}
-											</small>
-											{' - '}
-											<small className="time-class">{post.timeToRead} min.</small>
-										</p>
-										<p>
-											<span
-												style={{
-													fontSize: '16px',
-													textAlign: 'justify',
-													textJustify: 'inter-word'
-												}}
-											>
-												{post.excerpt}
-											</span>
+											<span className="ultimas-entradas">Últimas entradas</span>
 											<br />
-											<br />
-											<Link className="seguir-leyendo" to={post.fields.slug}>
-												Seguir leyendo →
-											</Link>
-										</p>
+										</div>
+										{posts.map(({ node: post }) => {
+											return (
+												<div className="box-post" key={post.id}>
+													<p>
+														<Link className="post-title-class" to={post.fields.slug}>
+															{post.frontmatter.title}
+														</Link>
+														<br />
+														<small className="date-class">
+															{new Intl.DateTimeFormat('es-GT', {
+																year: 'numeric',
+																month: 'long'
+															}).format(Date.parse(post.frontmatter.date))}
+														</small>
+														{' - '}
+														<small className="time-class">{post.timeToRead} min.</small>
+													</p>
+													<p>
+														<span
+															style={{
+																fontSize: '16px',
+																textAlign: 'justify',
+																textJustify: 'inter-word'
+															}}
+														>
+															{post.excerpt}
+														</span>
+														<br />
+														<br />
+														<Link className="seguir-leyendo" to={post.fields.slug}>
+															Seguir leyendo →
+														</Link>
+													</p>
+												</div>
+											);
+										})}
 									</div>
-								);
-							})}
-						</div>
-					</section>
-				)}
-			</Layout>
+								</section>
+							)}
+						</Layout>
+					);
+				}}
+			</Context.Consumer>
 		);
 	}
 }
