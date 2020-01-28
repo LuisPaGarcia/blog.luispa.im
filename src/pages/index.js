ad75e1b963dea470d391bbf96903ff6220eebe5b
LuisPa <diganluispa@gmail.com>
2020-01-27 21:23:44 -0600

    2020 UPDATE

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,14 @@ export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.up = (state) => {
+		this.up = state => {
 			this.setState({
 				visible: state
 			});
 		};
 
 		this.state = {
-			visible: 'about',
+			visible: 'blog',
 			up: this.up
 		};
 	}
@@ -42,72 +42,51 @@ class IndexPage extends React.Component {
 				{({ visible, up, posts }) => {
 					return (
 						<Layout>
-							<div className="topMenu">
-								<span
-									className={`item-menu-class ${visible === 'about' ? ` active` : ``}`}
-									onClick={() => up('about')}
-								>
-									About
-								</span>
-								<span
-									className={`item-menu-class ${visible === 'blog' ? ` active` : ``}`}
-									onClick={() => up('blog')}
-								>
-									Blog
-								</span>
-							</div>
-							{!(visible === 'about') ? null : (
-								<section>
-									<h1 className="pt-8">HOLA</h1>
-								</section>
-							)}
-							{!(visible === 'blog') ? null : (
-								<section>
+							<section>
+								<div>
 									<div>
-										<div>
-											<br />
-											<span className="ultimas-entradas">Últimas entradas</span>
-											<br />
-										</div>
-										{posts.map(({ node: post }) => {
-											return (
-												<div className="box-post" key={post.id}>
-													<p>
-														<Link className="post-title-class" to={post.fields.slug}>
-															{post.frontmatter.title}
-														</Link>
-														<br />
-														<small className="date-class">
-															{new Intl.DateTimeFormat('es-GT', {
-																year: 'numeric',
-																month: 'long'
-															}).format(Date.parse(post.frontmatter.date))}
-														</small>
-														{' - '}
-														<small className="time-class">{post.timeToRead} min.</small>
-													</p>
-													<p>
-														<span
-															style={{
-																fontSize: '16px',
-																textAlign: 'justify',
-																textJustify: 'inter-word'
-															}}
-														>
-															{post.excerpt}
-														</span>
-														<br />
-														<br />
-														<Link className="seguir-leyendo" to={post.fields.slug}>
-															Seguir leyendo →
-														</Link>
-													</p>
-												</div>
-											);
-										})}
+										<br />
+										<span className="ultimas-entradas">Últimas entradas</span>
+										<br />
 									</div>
-								</section>
-							)}
+									{posts.map(({ node: post }) => {
+										return (
+											<div className="box-post" key={post.id}>
+												<p>
+													<Link className="post-title-class" to={post.fields.slug}>
+														{post.frontmatter.title}
+													</Link>
+													<br />
+													<small className="date-class">
+														{new Intl.DateTimeFormat('es-GT', {
+															year: 'numeric',
+															month: 'long'
+														}).format(Date.parse(post.frontmatter.date))}
+													</small>
+													{' - '}
+													<small className="time-class">{post.timeToRead} min.</small>
+												</p>
+												<p>
+													<span
+														style={{
+															fontSize: '16px',
+															textAlign: 'justify',
+															textJustify: 'inter-word'
+														}}
+													>
+														{post.excerpt}
+													</span>
+													<br />
+													<br />
+													<Link className="seguir-leyendo" to={post.fields.slug}>
+														Seguir leyendo →
+													</Link>
+												</p>
+											</div>
+										);
+									})}
+								</div>
+							</section>
 						</Layout>
 					);
 				}}
