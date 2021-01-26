de4ca11a38b5fc81e5358309ab9cf7e64be93bed
LuisPa <diganluispa@gmail.com>
2021-01-25 19:21:47 -0600

    refactor: Delete all CSS import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,130 +1,137 @@
 //@ts-check
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import '../style.css';
-import { Context } from '../context';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import { Context } from "../context";
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.up = state => {
-			this.setState({
-				visible: state
-			});
-		};
+    this.up = (state) => {
+      this.setState({
+        visible: state,
+      });
+    };
 
-		this.state = {
-			visible: 'blog',
-			up: this.up
-		};
-	}
+    this.state = {
+      visible: "blog",
+      up: this.up,
+    };
+  }
 
-	render() {
-		let value = {
-			visible: this.state.visible,
-			up: this.state.up,
-			posts: this.props.data.allMarkdownRemark.edges
-		};
-		return (
-			<Context.Provider value={value}>
-				<IndexPage />
-			</Context.Provider>
-		);
-	}
+  render() {
+    let value = {
+      visible: this.state.visible,
+      up: this.state.up,
+      posts: this.props.data.allMarkdownRemark.edges,
+    };
+    return (
+      <Context.Provider value={value}>
+        <IndexPage />
+      </Context.Provider>
+    );
+  }
 }
 
 class IndexPage extends React.Component {
-	render() {
-		return (
-			<Context.Consumer>
-				{({ visible, up, posts }) => {
-					return (
-						<Layout>
-							<section>
-								<div>
-									<div>
-										<br />
-										<span className="ultimas-entradas">Últimas entradas</span>
-										<br />
-									</div>
-									{posts.map(({ node: post }) => {
-										return (
-											<div className="box-post" key={post.id}>
-												<p>
-													<Link className="post-title-class" to={post.fields.slug}>
-														{post.frontmatter.title}
-													</Link>
-													<br />
-													<small className="date-class">
-														{new Intl.DateTimeFormat('es-GT', {
-															year: 'numeric',
-															month: 'long'
-														}).format(Date.parse(post.frontmatter.date))}
-													</small>
-													{' - '}
-													<small className="time-class">{post.timeToRead} min.</small>
-												</p>
-												<p>
-													<span
-														style={{
-															fontSize: '16px',
-															textAlign: 'justify',
-															textJustify: 'inter-word'
-														}}
-													>
-														{post.excerpt}
-													</span>
-													<br />
-													<br />
-													<Link className="seguir-leyendo" to={post.fields.slug}>
-														Seguir leyendo →
-													</Link>
-												</p>
-											</div>
-										);
-									})}
-								</div>
-							</section>
-						</Layout>
-					);
-				}}
-			</Context.Consumer>
-		);
-	}
+  render() {
+    return (
+      <Context.Consumer>
+        {({ visible, up, posts }) => {
+          return (
+            <Layout>
+              <section>
+                <div>
+                  <div>
+                    <br />
+                    <span className="ultimas-entradas">Últimas entradas</span>
+                    <br />
+                  </div>
+                  {posts.map(({ node: post }) => {
+                    return (
+                      <div className="box-post" key={post.id}>
+                        <p>
+                          <Link
+                            className="post-title-class"
+                            to={post.fields.slug}
+                          >
+                            {post.frontmatter.title}
+                          </Link>
+                          <br />
+                          <small className="date-class">
+                            {new Intl.DateTimeFormat("es-GT", {
+                              year: "numeric",
+                              month: "long",
+                            }).format(Date.parse(post.frontmatter.date))}
+                          </small>
+                          {" - "}
+                          <small className="time-class">
+                            {post.timeToRead} min.
+                          </small>
+                        </p>
+                        <p>
+                          <span
+                            style={{
+                              fontSize: "16px",
+                              textAlign: "justify",
+                              textJustify: "inter-word",
+                            }}
+                          >
+                            {post.excerpt}
+                          </span>
+                          <br />
+                          <br />
+                          <Link
+                            className="seguir-leyendo"
+                            to={post.fields.slug}
+                          >
+                            Seguir leyendo →
+                          </Link>
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </section>
+            </Layout>
+          );
+        }}
+      </Context.Consumer>
+    );
+  }
 }
 
 IndexPage.propTypes = {
-	data: PropTypes.shape({
-		allMarkdownRemark: PropTypes.shape({
-			edges: PropTypes.array
-		})
-	})
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
 };
 
 export const pageQuery = graphql`
-	query IndexQuery {
-		allMarkdownRemark(
-			sort: { order: DESC, fields: [frontmatter___date] }
-			filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-		) {
-			edges {
-				node {
-					excerpt(pruneLength: 100)
-					id
-					fields {
-						slug
-					}
-					timeToRead
-					htmlAst
-					frontmatter {
-						title
-						templateKey
-						date
-					}
-				}
-			}
-		}
-	}
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 100)
+          id
+          fields {
+            slug
+          }
+          timeToRead
+          htmlAst
+          frontmatter {
+            title
+            templateKey
+            date
+          }
+        }
+      }
+    }
+  }
 `;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,83 +1,92 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { kebabCase } from 'lodash';
-import Helmet from 'react-helmet';
-import { graphql, Link } from 'gatsby';
-import Layout from '../components/Layout';
-import Content, { HTMLContent } from '../components/Content';
-import Navbar from '../components/Navbar';
+import React from "react";
+import PropTypes from "prop-types";
+import { kebabCase } from "lodash";
+import Helmet from "react-helmet";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
+import Navbar from "../components/Navbar";
 
-import '../style.css';
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet,
+}) => {
+  const PostContent = contentComponent || Content;
 
-export const BlogPostTemplate = ({ content, contentComponent, description, tags, title, helmet }) => {
-	const PostContent = contentComponent || Content;
-
-	return (
-		<div>
-			{helmet || ''}
-			<h1 className="title-class">{title}</h1>
-			<p>{description}</p>
-			<PostContent content={content} />
-			{tags && tags.length ? (
-				<div style={{ marginTop: `4rem` }}>
-					<h3>Tags</h3>
-					{tags.map((tag) => (
-						<Link className="tag-class" key={tag + `tag`} to={`/tags/${kebabCase(tag)}/`}>
-							{tag}
-						</Link>
-					))}
-				</div>
-            ) : null}
-            <br/>
-			<Navbar text="Volver" />
-		</div>
-	);
+  return (
+    <div>
+      {helmet || ""}
+      <h1 className="title-class">{title}</h1>
+      <p>{description}</p>
+      <PostContent content={content} />
+      {tags && tags.length ? (
+        <div style={{ marginTop: `4rem` }}>
+          <h3>Tags</h3>
+          {tags.map((tag) => (
+            <Link
+              className="tag-class"
+              key={tag + `tag`}
+              to={`/tags/${kebabCase(tag)}/`}
+            >
+              {tag}
+            </Link>
+          ))}
+        </div>
+      ) : null}
+      <br />
+      <Navbar text="Volver" />
+    </div>
+  );
 };
 
 BlogPostTemplate.propTypes = {
-	content: PropTypes.node.isRequired,
-	contentComponent: PropTypes.func,
-	description: PropTypes.string,
-	title: PropTypes.string,
-	helmet: PropTypes.instanceOf(Helmet)
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.instanceOf(Helmet),
 };
 
 const BlogPost = ({ data }) => {
-	const { markdownRemark: post } = data;
+  const { markdownRemark: post } = data;
 
-	return (
-		<Layout>
-			<BlogPostTemplate
-				content={post.html}
-				contentComponent={HTMLContent}
-				description={post.frontmatter.description}
-				helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
-				tags={post.frontmatter.tags}
-				title={post.frontmatter.title}
-			/>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <BlogPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+        tags={post.frontmatter.tags}
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  );
 };
 
 BlogPost.propTypes = {
-	data: PropTypes.shape({
-		markdownRemark: PropTypes.object
-	})
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
 };
 
 export default BlogPost;
 
 export const pageQuery = graphql`
-	query BlogPostByID($id: String!) {
-		markdownRemark(id: { eq: $id }) {
-			id
-			html
-			frontmatter {
-				date(formatString: "MMMM DD, YYYY")
-				title
-				description
-				tags
-			}
-		}
-	}
+  query BlogPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+        tags
+      }
+    }
+  }
 `;
