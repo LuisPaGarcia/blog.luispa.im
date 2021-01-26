ba33563243b791e0a1847f1707b04dbc1daa75b2
LuisPa <diganluispa@gmail.com>
2021-01-25 19:18:29 -0600

    refactor: Delete all styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import '../style.css';
-import PageTransition from 'gatsby-v2-plugin-page-transitions';
-
-import Navbar from '../components/Navbar';
-import './all.sass';
+import React from "react";
+import Helmet from "react-helmet";
+import PageTransition from "gatsby-v2-plugin-page-transitions";
+import Navbar from "../components/Navbar";
 
 const TemplateWrapper = (props) => {
-	return (
-		<div>
-			<Helmet title="Luispa.im" />
-			<Navbar text="luispa.im" />
-			<PageTransition>
-				<div>{props.children}</div>
-				<br />
-				<br />
-			</PageTransition>
-		</div>
-	);
+  return (
+    <div>
+      <Helmet title="Luispa.im" />
+      <Navbar text="luispa.im" />
+      <PageTransition>
+        <div>{props.children}</div>
+        <br />
+        <br />
+      </PageTransition>
+    </div>
+  );
 };
 
 export default TemplateWrapper;
