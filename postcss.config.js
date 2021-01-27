02a35c842833f2a2080ba43d05116158a24b4d3d
LuisPa <diganluispa@gmail.com>
2021-01-26 19:54:45 -0600

    feat: Update Tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-const tailwindcss = require('tailwindcss')
-
+// postcss.config.js
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
